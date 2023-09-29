4551ceae62e941255a98bab6f484e29ec6307618
unknown <raavi.yelpula07@gmail.com>
2023-09-30 02:04:18 +0530

    updates on menus, responsivesness

--- validation.js
+++ validation.js
@@ -1,26 +1,85 @@
 
-    function setTitle(){
-        const title2 = new Map([
-            ["ips1","Innovation Support"],
-            ["ips2","IP Protection"],
-            ["ips3","Enforcement and Commercialization"],
-            ["ips4","IP Portfolio Management"],
-            ["ips5","Market Research"],
-            ["ips6","Busniess Development"]
-            ]);
-        const desc = new Map([
-              ["ips1", "<li>Quality patent and non-patent prior art search, analysis</li> <li>Explore and identify the emerging technologies and new opportunities</li><li>Understand the existing prior art around the new ideas to pursue further development</li> <li>Landscapes and identification of white space to determine the area to focus and develop <br>long term technology roadmap</li>"],
-              ["ips2", "<li>Identification the potential inventions, strategically protect them by high quality patents</li> <li>Patentability analysis to increase the success of rate of patent grant</li> <li>Drat and file quality patent application</li> <li>Effective monitoring and office action to secure early and cost-effective protection</li> <li>Strategic advice on patent filing in different jurisdictions of business interest</li>"],
-              ["ips3", "<li>Explore the opportunities for diverse solutions from the protected IP assets of the client</li> <li>Protect the clients from the costly IP litigation with efficient and timely due diligence</li> <li>Validity and invalidity analysis for IP assets under litigation and potential for litigation</li> <li>Explore the licensing, cross licensing and partnership opportunities</li>"],
-              ["ips4", "<li>Develop IP policy, processes, strategy for effective management of IP assets to maximize the benefits.</li> <li>Audit and evaluation organization IP assets on regular basis</li> <li>Monitor and manage the legal requirements, maintain healthy and commercially viable IP portfolio</li> <li>Timely advice to top management, business and technology teams on latest trends and legal updates </li>"],
-              ["ips5", "<li>We provide comprehensive competitive intelligence reports on the trends and forecast on <br>technology, products, market, and competitor. </li> <li>Forecast the products and services trends and opportunities to stay ahead of competition</li>"],
-              ["ips6", "<li>IP Risk assessment for merger, acquisitions, licensing and strategic sell off</li> <li>Technology Scouting</li>"]
-            ]);
-        var ur=window.location.href.split("#")[1];
-        location.reload();
-    document.getElementById("servtitle").innerHTML = title2.get(ur);
-    console.log("URL: "+ur);
-    document.getElementById("servdesc").innerHTML = desc.get(ur);
-    }
-    
+    // function setTitle(){
+    //     const title2 = new Map([
+    //         ["ips1","Innovation Support"],
+    //         ["ips2","IP Protection"],
+    //         ["ips3","Enforcement and Commercialization"],
+    //         ["ips4","IP Portfolio Management"],
+    //         ["ips5","Market Research"],
+    //         ["ips6","Busniess Development"]
+    //         ]);
+    //     const desc = new Map([
+    //           ["ips1", "<li>Quality patent and non-patent prior art search, analysis</li> <li>Explore and identify the emerging technologies and new opportunities</li><li>Understand the existing prior art around the new ideas to pursue further development</li> <li>Landscapes and identification of white space to determine the area to focus and develop <br>long term technology roadmap</li>"],
+    //           ["ips2", "<li>Identification the potential inventions, strategically protect them by high quality patents</li> <li>Patentability analysis to increase the success of rate of patent grant</li> <li>Drat and file quality patent application</li> <li>Effective monitoring and office action to secure early and cost-effective protection</li> <li>Strategic advice on patent filing in different jurisdictions of business interest</li>"],
+    //           ["ips3", "<li>Explore the opportunities for diverse solutions from the protected IP assets of the client</li> <li>Protect the clients from the costly IP litigation with efficient and timely due diligence</li> <li>Validity and invalidity analysis for IP assets under litigation and potential for litigation</li> <li>Explore the licensing, cross licensing and partnership opportunities</li>"],
+    //           ["ips4", "<li>Develop IP policy, processes, strategy for effective management of IP assets to maximize the benefits.</li> <li>Audit and evaluation organization IP assets on regular basis</li> <li>Monitor and manage the legal requirements, maintain healthy and commercially viable IP portfolio</li> <li>Timely advice to top management, business and technology teams on latest trends and legal updates </li>"],
+    //           ["ips5", "<li>We provide comprehensive competitive intelligence reports on the trends and forecast on <br>technology, products, market, and competitor. </li> <li>Forecast the products and services trends and opportunities to stay ahead of competition</li>"],
+    //           ["ips6", "<li>IP Risk assessment for merger, acquisitions, licensing and strategic sell off</li> <li>Technology Scouting</li>"]
+    //         ]);
+    //     var ur=window.location.href.split("#")[1];
+    //     location.reload();
+    // document.getElementById("servtitle").innerHTML = title2.get(ur);
+    // console.log("URL: "+ur);
+    // document.getElementById("servdesc").innerHTML = desc.get(ur);
+    // }
+    // let i=9;
+    // // 'use strict';
+    // function f1(){
+    //     this.i=10;
+    //     // console.log(this);
+    // }
+    // f1();
+    // console.log(this.i);
 
+
+    // <!--
+    // <div class="w3-container ipservices" id="ips3">
+    //   <h3>Enforcement and Commercialization</h3>
+    //   <div>
+    //     <li>Explore the opportunities for diverse solutions from the protected IP assets of the client</li>
+    //     <li>Protect the clients from the costly IP litigation with efficient and timely due diligence</li>
+    //     <li>Validity and invalidity analysis for IP assets under litigation and potential for litigation</li>
+    //     <li>Explore the licensing, cross licensing and partnership opportunities</li>
+    //   </div>
+    //   <div id="sfooterNote">
+    //     <a href="#navBar"><i class="fa fa-caret-up w3-xxxlarge" style="float: right;color: red;font-size: 3px;"></i></a>
+    //   </div>
+    // </div>
+
+    // <div class="w3-container ipservices" id="ips4">
+    //   <h3>IP Portfolio Management</h3>
+    //   <div>
+    //     <li>Develop IP policy, processes, strategy for effective management of IP assets to maximize the benefits.</li>
+    //     <li>Audit and evaluation organization IP assets on regular basis</li>
+    //     <li>Monitor and manage the legal requirements, maintain healthy and commercially viable IP portfolio</li>
+    //     <li>Timely advice to top management, business and technology teams on latest trends and legal updates </li>
+    //   </div>
+    //   <div id="sfooterNote">
+    //     <a href="#navBar"><i class="fa fa-caret-up w3-xxxlarge" style="float: right;color: red;font-size: 3px;"></i></a>
+    //   </div>
+    // </div>
+
+    // <div class="w3-container ipservices" id="ips5">
+    //   <h3 id="ips5title">Market Research</h3>
+    //   <div>
+    //     <li>We provide comprehensive competitive intelligence reports on the trends and forecast on technology,
+    //       products, market, and competitor. </li>
+    //     <li>Forecast the products and services trends and opportunities to stay ahead of competition</li>
+    //     <div id="sfooterNote">
+    //       <a href="#navBar"><i class="fa fa-caret-up w3-xxxlarge"
+    //           style="float: right;color: red;font-size: 3px;"></i></a>
+    //     </div>
+    //   </div>
+
+    //   <div class="w3-container ipservices" id="ips6">
+    //     <h3>Busniess Development</h3>
+    //     <div>
+    //       <li>IP Risk assessment for merger, acquisitions, licensing and strategic sell off</li>
+    //       <li>Technology Scouting</li>
+    //     </div>
+    //     <div id="sfooterNote">
+    //       <a href="#navBar"><i class="fa fa-caret-up w3-xxxlarge"
+    //           style="float: right;color: red;font-size: 3px;"></i></a>
+    //     </div>
+    //   </div> 
+    // -->
